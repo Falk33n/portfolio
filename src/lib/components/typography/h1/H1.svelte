ee8a8ad4ff02da43a8613a41299f677c307ac742
Falk33n <tim.falk00@gmail.com>
2024-12-07 22:50:32 +0100

    style: change font size

--- src/lib/components/typography/h1/H1.svelte
+++ src/lib/components/typography/h1/H1.svelte
@@ -7,7 +7,7 @@
 
 <h1
 	class={cn(
-		'scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl',
+		'scroll-m-20 text-5xl font-extrabold tracking-tight md:text-6xl',
 		className,
 	)}
 	{...props}
