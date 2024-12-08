2542d8be6a1dacbe25587bfd8854a94d7d696f2b
Falk33n <tim.falk00@gmail.com>
2024-12-08 20:13:50 +0100

    feat: add links for in-page navigation

---
+++ src/lib/components/sections/layout/SectionLink.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { HTMLAnchorAttributes } from 'svelte/elements';
+
+	type Props = HTMLAnchorAttributes;
+
+	// Will be used later
+	// let isCurrentSection = $state(false);
+
+	let { children, ...props }: Props = $props();
+</script>
+
+<a
+	class="group flex max-w-max items-center gap-4"
+	{...props}
+>
+	<span
+		class="h-px w-14 bg-foreground/60 transition-all group-hover:w-20 group-hover:bg-foreground group-focus-visible:w-20 group-focus-visible:bg-foreground"
+	></span>
+	<span class="text-sm font-bold uppercase tracking-widest">
+		{@render children?.()}
+	</span>
+</a>
