c8e6cdd3c6341fcd12d0d1cb9693fdca048c1920
Falk33n <tim.falk00@gmail.com>
2024-12-08 14:36:56 +0100

    feat: add new icons

--- src/lib/components/icons/index.ts
+++ src/lib/components/icons/index.ts
@@ -1,8 +1,11 @@
 import type { SVGAttributes } from 'svelte/elements';
+import CommentShare from './CommentShare.svelte';
+import FilePencil from './FilePencil.svelte';
 import Github from './Github.svelte';
+import LinkedIn from './LinkedIn.svelte';
 import MoonHalf from './MoonHalf.svelte';
 import Sun from './Sun.svelte';
 
 export type IconProps = SVGAttributes<SVGSVGElement>;
 
-export { Github, MoonHalf, Sun };
+export { CommentShare, FilePencil, Github, LinkedIn, MoonHalf, Sun };
