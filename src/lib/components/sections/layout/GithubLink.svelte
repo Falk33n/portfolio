4e706c99c19cc69fd357ae37f6cf9155b24e59c3
Falk33n <tim.falk00@gmail.com>
2024-12-08 09:52:03 +0100

    feat: make seperate component of github link

---
+++ src/lib/components/sections/layout/GithubLink.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import {
+		Tooltip,
+		TooltipContent,
+		TooltipProvider,
+		TooltipTrigger,
+	} from '$components/feedback';
+	import { Button as Link } from '$components/forms';
+	import { Github } from '$components/icons';
+
+	const githubTitle = "Go to Tim Falk's GitHub profile, opens in a new tab";
+</script>
+
+<TooltipProvider>
+	<Tooltip>
+		<TooltipTrigger>
+			{#snippet child({ props })}
+				<Link
+					href="https://github.com/Falk33n"
+					aria-label={githubTitle}
+					variant="outline"
+					size="icon"
+					target="_blank"
+					{...props}
+				>
+					<Github class="scale-125" />
+				</Link>
+			{/snippet}
+		</TooltipTrigger>
+		<TooltipContent>
+			{githubTitle}
+		</TooltipContent>
+	</Tooltip>
+</TooltipProvider>
--- src/lib/components/sections/layout/index.ts
+++ src/lib/components/sections/layout/index.ts
@@ -1,5 +1,6 @@
+import GithubLink from './GithubLink.svelte';
 import Header from './Header.svelte';
 import Logo from './Logo.svelte';
 import SocialMediaNav from './SocialMediaNav.svelte';
 
-export { Header, Logo, SocialMediaNav };
+export { GithubLink, Header, Logo, SocialMediaNav };
