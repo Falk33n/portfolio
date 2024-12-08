7ea109d1de70a7dd8481defcd23e5870de62609a
Falk33n <tim.falk00@gmail.com>
2024-12-08 14:37:48 +0100

    feat: add social media links

--- src/lib/components/sections/layout/SocialMediaNav.svelte
+++ src/lib/components/sections/layout/SocialMediaNav.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
 	import { ModeToggleButton } from '$components/forms';
-	import { GithubLink } from '$components/sections/layout';
+	import {
+		GithubLink,
+		LinkedInLink,
+		ResumeLink,
+	} from '$components/sections/layout';
+	import ContactLink from './ContactLink.svelte';
 </script>
 
-<nav>
-	<GithubLink />
+<div class="mt-16 flex max-w-xs flex-wrap items-center justify-between gap-4">
+	<nav class="flex flex-wrap items-center gap-4">
+		<GithubLink />
+		<LinkedInLink />
+		<ResumeLink />
+		<ContactLink />
+	</nav>
 
 	<ModeToggleButton />
-</nav>
+</div>
