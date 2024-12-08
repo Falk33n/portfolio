2eb42ddd0151b843317474826bf54bc390adf833
Falk33n <tim.falk00@gmail.com>
2024-12-08 20:14:11 +0100

    style: add separator for decoration purposes

--- src/lib/components/sections/layout/SocialMediaNav.svelte
+++ src/lib/components/sections/layout/SocialMediaNav.svelte
@@ -1,20 +1,27 @@
 <script lang="ts">
 	import { ModeToggleButton } from '$components/forms';
 	import {
+		ContactLink,
 		GithubLink,
 		LinkedInLink,
 		ResumeLink,
 	} from '$components/sections/layout';
-	import ContactLink from './ContactLink.svelte';
 </script>
 
-<div class="mt-16 flex max-w-xs flex-wrap items-center justify-between gap-4">
-	<nav class="flex flex-wrap items-center gap-4">
+<div class="mt-10 flex max-w-xs flex-wrap items-center gap-4">
+	<nav
+		aria-label="Social media and other navigation links"
+		class="flex flex-wrap items-center gap-4"
+	>
 		<GithubLink />
 		<LinkedInLink />
 		<ResumeLink />
 		<ContactLink />
 	</nav>
 
+	<span
+		aria-hidden="true"
+		class="h-8 w-px bg-muted"
+	></span>
 	<ModeToggleButton />
 </div>
