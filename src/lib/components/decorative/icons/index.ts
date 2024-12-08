9247375325f25b56f0161b076400537bca79f0cd
Falk33n <tim.falk00@gmail.com>
2024-12-08 09:29:03 +0100

    feat: add icons from lineicons

---
+++ src/lib/components/decorative/icons/index.ts
@@ -0,0 +1,8 @@
+import type { SVGAttributes } from 'svelte/elements';
+import Github from './Github.svelte';
+import MoonHalf from './MoonHalf.svelte';
+import Sun from './Sun.svelte';
+
+export type IconProps = SVGAttributes<SVGSVGElement>;
+
+export { Github, MoonHalf, Sun };
