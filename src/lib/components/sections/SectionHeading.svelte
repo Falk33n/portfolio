9c0e150bf2f19dba60ece2ed4f78f1bdf0836c0a
Falk33n <tim.falk00@gmail.com>
2024-12-13 19:20:41 +0100

    feat: add SectionHeading component

---
+++ src/lib/components/sections/SectionHeading.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { H3 } from '$components/typography';
+
+	let { children } = $props();
+</script>
+
+<H3
+	class="w-full py-4 text-sm uppercase tracking-widest backdrop-blur-sm max-lg:sticky max-lg:top-0  lg:sr-only"
+>
+	{@render children?.()}
+</H3>
