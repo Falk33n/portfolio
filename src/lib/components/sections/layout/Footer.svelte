9ae982d544d96fa4af162efa9cba49f560d930eb
Falk33n <tim.falk00@gmail.com>
2024-12-13 19:20:28 +0100

    chore: rename FooterLink to Link

--- src/lib/components/sections/layout/Footer.svelte
+++ src/lib/components/sections/layout/Footer.svelte
@@ -1,40 +1,39 @@
 <script lang="ts">
-	import { FooterLink } from '$components/sections/layout';
+	import { Link } from '$components/sections';
 </script>
 
 <footer class="flex flex-1 flex-col justify-end">
 	<p class="max-w-sm text-sm text-foreground/80">
-		Coded in
-		<FooterLink
+		Coded by Tim Falk in
+		<Link
 			href="https://code.visualstudio.com/"
 			aria-label="Go to the Visual Studio Code website, opens in a new tab"
 		>
 			Visual Studio Code
-		</FooterLink>
-		by Tim Falk, built with
-		<FooterLink
+		</Link>, built using
+		<Link
 			href="https://svelte.dev/docs/kit/introduction"
 			aria-label="Go to the SvelteKit website, opens in a new tab"
 		>
 			SvelteKit
-		</FooterLink>, designed with
-		<FooterLink
+		</Link>, and designed with
+		<Link
 			href="https://tailwindcss.com/"
 			aria-label="Go to the Tailwind CSS website, opens in a new tab"
 		>
 			Tailwind CSS
-		</FooterLink>, deployed with
-		<FooterLink
+		</Link>. Deployed via
+		<Link
 			href="https://vercel.com/"
 			aria-label="Go to the Vercel website, opens in a new tab"
 		>
 			Vercel
-		</FooterLink>. All text is set in the
-		<FooterLink
+		</Link>, with all text styled in the
+		<Link
 			href="https://vercel.com/font"
 			aria-label="Go to the Geist-Sans website, opens in a new tab"
 		>
 			Geist-Sans
-		</FooterLink> typeface.
+		</Link> typeface.
 	</p>
 </footer>
