f1ae1c4e0e9019f5f0cd75263547406e9acec855
Falk33n <tim.falk00@gmail.com>
2024-12-13 18:44:51 +0100

    style: disable gloweffect on light mode

--- src/lib/components/decorative/GlowEffect.svelte
+++ src/lib/components/decorative/GlowEffect.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { cn } from '$utils';
+	import { mode } from 'mode-watcher';
 	import { onMount } from 'svelte';
 
 	function updateMousePosition(e: MouseEvent) {
@@ -38,23 +39,25 @@
 	});
 </script>
 
-<div
-	aria-hidden="true"
-	class="pointer-events-none fixed left-0 top-0 z-[9999] size-full"
->
+{#if $mode === 'dark'}
 	<div
 		aria-hidden="true"
-		class={cn(
-			'custom-transition custom-background pointer-events-none -translate-y-1/2 rounded-full opacity-10  mix-blend-soft-light',
-			isTouchDevice
-				? 'fixed size-[650px]'
-				: 'absolute size-[800px] -translate-x-1/2',
-		)}
-		style={isTouchDevice
-			? `top: calc(50% + ${scrollY}px); left: -200px;`
-			: `top: ${mousePosition.y}px; left: ${mousePosition.x}px;`}
-	></div>
-</div>
+		class="pointer-events-none fixed left-0 top-0 z-[9999] size-full"
+	>
+		<div
+			aria-hidden="true"
+			class={cn(
+				'custom-transition custom-background pointer-events-none -translate-y-1/2 rounded-full opacity-10  mix-blend-soft-light',
+				isTouchDevice
+					? 'fixed size-[650px]'
+					: 'absolute size-[800px] -translate-x-1/2',
+			)}
+			style={isTouchDevice
+				? `top: calc(50% + ${scrollY}px); left: -200px;`
+				: `top: ${mousePosition.y}px; left: ${mousePosition.x}px;`}
+		></div>
+	</div>
+{/if}
 
 <style>
 	.custom-transition {
